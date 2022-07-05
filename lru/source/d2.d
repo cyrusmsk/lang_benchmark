910a73e57211fd26f47ab2abb7679bd6546bc051
Serg Gini <kornburn@gmail.com>
2022-07-05 23:30:56 +0300

    Clean old results

--- lru/source/d2.d
+++ lru/source/d2.d
@@ -2,37 +2,35 @@
 import std;
 
 // std.container.dlist does not provide suitable APIs
-class LinkedListNode(T)
+struct Node(T)
 {
     T data;
-    LinkedListNode!T prev;
-    LinkedListNode!T next;
-
-    this(T data)
+    Node* next;
+    Node* prev;
+    
+    this(T d)
     {
-        this.data = data;
+        this.data = d;
     }
 }
 
-class LinkedList(T)
+final class LinkedList(T)
 {
     int len = 0;
-    LinkedListNode!T head;
-    LinkedListNode!T tail;
+    Node!(T)* head = null;
+    Node!(T)* tail = null;
 
-    this()
-    {
-    }
+    this() {}
 
-    LinkedListNode!(T) add(T data)
+    Node!(T)* add(T data)
     {
-        auto node = new LinkedListNode!(T)(data);
+        auto node = new Node!(T)(data);
         __add_node(node);
         len += 1;
         return node;
     }
 
-    void __add_node(LinkedListNode!(T) node)
+    void __add_node(Node!(T)* node)
     {
         if (head is null)
         {
@@ -47,7 +45,7 @@ class LinkedList(T)
         node.next = null;
     }
 
-    void __remove(LinkedListNode!(T) node)
+    void __remove(Node!(T)* node)
     {
         if (head is node)
         {
@@ -67,22 +65,23 @@ class LinkedList(T)
         }
     }
 
-    void move_to_end(LinkedListNode!(T) node)
+    void move_to_end(Node!(T)* node)
     {
         __remove(node);
         __add_node(node);
     }
 }
 
-class LRU(TK, TV)
+struct LRU(TK, TV)
 {
     int size;
-    LinkedListNode!(Tuple!(TK, TV))[TK] _key_lookup;
+    Node!(Tuple!(TK, TV))*[TK] _key_lookup;
     LinkedList!(Tuple!(TK, TV)) _entries;
+
     this(int size)
     {
         this.size = size;
-        _entries = new LinkedList!(Tuple!(TK, TV))();
+        _entries = new LinkedList!(Tuple!(TK, TV));
     }
 
     Nullable!TV get(TK key)
@@ -118,7 +117,7 @@ class LRU(TK, TV)
     }
 }
 
-class LCG
+static struct LCG
 {
     uint seed;
     this(uint seed)
@@ -135,6 +134,7 @@ class LCG
 
 void main(string[] args)
 {
+    //import core.memory; 
     auto size = args.length > 1 ? args[1].to!int() : 100;
     auto n = args.length > 2 ? args[2].to!int() : 100;
     auto mod = size * 10;
@@ -143,11 +143,12 @@ void main(string[] args)
     auto hit = 0;
     auto missed = 0;
     auto lru = new LRU!(uint, uint)(size);
+    //GC.disable();
     foreach (int i; 0 .. n)
     {
         auto n0 = rng0.next() % mod;
-        lru.put(n0, n0);
         auto n1 = rng1.next() % mod;
+        lru.put(n0, n0);
         if (lru.get(n1).isNull)
         {
             missed += 1;
@@ -157,7 +158,6 @@ void main(string[] args)
             hit += 1;
         }
     }
-
     writefln("%d\n%d", hit, missed);
 }
 
